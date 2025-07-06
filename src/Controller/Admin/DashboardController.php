322a2b85c7a7bb8327145953797fe32dca301645
Fabien Potencier <fabien@potencier.org>
2025-07-06 22:35:15 +0200

    Add 9.2: Configuring EasyAdmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    public function index(): Response
+    {
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(ConferenceCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // 1.1) If you have enabled the "pretty URLs" feature:
+        // return $this->redirectToRoute('admin_user_index');
+        //
+        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirectToRoute('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Guestbook Generated');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
+        yield MenuItem::linkToCrud('Conferences', 'fas fa-map-marker-alt', Conference::class);
+        yield MenuItem::linkToCrud('Comments', 'fas fa-comments', Comment::class);
+    }
+}
